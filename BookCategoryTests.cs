2e13e84f9da4d5a8af862e344010d82f5230cc32
Yordan <143961654+yordanthedon@users.noreply.github.com>
2024-12-20 21:37:57 +0200

    Add files via upload

---
+++ BookCategoryTests.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+
+namespace ApiTests
+{
+    [TestFixture]
+    public class BookCategoryTests : IDisposable
+    {
+        private RestClient client;
+        private string token;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient(GlobalConstants.BaseUrl);
+            token = GlobalConstants.AuthenticateUser("john.doe@example.com", "password123");
+
+            Assert.That(token, Is.Not.Null.Or.Empty, "Authentication token should not be null or empty");
+        }
+
+        [Test]
+        public void Test_BookCategoryLifecycle()
+        {
+            // Step 1: Create a new book category
+            var createCategoryRequest = new RestRequest("/category", Method.Post);
+            createCategoryRequest.AddHeader("Authorization", $"Bearer {token}");
+
+            createCategoryRequest.AddJsonBody(new
+            {
+                title = "Fictional Literature"
+            });
+
+            var createResponse = client.Execute(createCategoryRequest);
+
+            Assert.That(createResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var createdCategory = JObject.Parse(createResponse.Content);
+            var categoryId = createdCategory["_id"]?.ToString();
+            var newTitle = createdCategory["title"]?.ToString();
+
+            Assert.That(categoryId, Is.Not.Null.Or.Empty);
+            Assert.That(newTitle, Is.EqualTo("Fictional Literature"));
+
+            // Step 2: Retrieve all book categories and verify the newly created category is present
+            var getAllCategories = new RestRequest("/category", Method.Get);
+            var getAllCategoriesResponse = client.Execute(getAllCategories);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(getAllCategoriesResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+                Assert.That(getAllCategoriesResponse.Content, Is.Not.Empty);
+
+                var categories = JArray.Parse(getAllCategoriesResponse.Content);
+
+
+                Assert.That(categories.Type, Is.EqualTo(JTokenType.Array));
+
+                Assert.That(categories.Count, Is.GreaterThan(0));
+
+                var createdCategory = categories.FirstOrDefault(c => c["title"]?.ToString() == "Fictional Literature");
+
+                Assert.That(createdCategory, Is.Not.Null);
+
+                var verifyId = categories.FirstOrDefault(c => c["_id"]?.ToString() == categoryId);
+
+                Assert.That(verifyId, Is.Not.Null);
+                Assert.That(categoryId, Is.EqualTo(verifyId["_id"]?.ToString()));
+            });
+
+            // Step 3: Update the category title
+            var editReqsuest = new RestRequest($"/category/{categoryId}", Method.Put);
+            editReqsuest.AddHeader("Authorization", $"Bearer {token}");
+            editReqsuest.AddJsonBody(new
+            {
+                title = "Updated Fictional Literature"
+            });
+            var editResponse = client.Execute(editReqsuest);
+            Assert.That(editResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            // Step 4: Verify that the category details have been updated
+            var getEditedCategoryRequset = new RestRequest($"/category/{categoryId}", Method.Get);
+            var getEditedCategoryResponse = client.Execute(getEditedCategoryRequset);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(getEditedCategoryResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(getEditedCategoryResponse.Content, Is.Not.Empty);
+
+                var updatedCategoryGet = JObject.Parse(getEditedCategoryResponse.Content);
+                Assert.That(updatedCategoryGet["title"]?.ToString(), Is.EqualTo("Updated Fictional Literature"));
+            });
+
+            // Step 5: Delete the category and validate it's no longer accessible
+            var deleteCategory = new RestRequest($"/category/{categoryId}", Method.Delete);
+            deleteCategory.AddHeader("Authorization", $"Bearer {token}");
+            var deleteResponse = client.Execute(deleteCategory);
+
+            Assert.That(deleteResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            // Step 6: Verify that the deleted category cannot be found
+            var verifyDeleteReqsuset = new RestRequest($"/category/{categoryId}", Method.Get);
+            var verifyDeleteResponse = client.Execute(verifyDeleteReqsuset);
+
+            Assert.That(verifyDeleteResponse.Content, Is.EqualTo("null"));
+        }
+
+        public void Dispose()
+        {
+            client?.Dispose();
+        }
+    }
+}
---
+++ BookTests.cs
@@ -0,0 +1,216 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Net;
+
+namespace ApiTests
+{
+    [TestFixture]
+    public class BookTests : IDisposable
+    {
+        private RestClient client;
+        private string token;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new RestClient(GlobalConstants.BaseUrl);
+            token = GlobalConstants.AuthenticateUser("john.doe@example.com", "password123");
+
+            Assert.That(token, Is.Not.Null.Or.Empty, "Authentication token should not be null or empty");
+        }
+
+        [Test]
+        public void Test_GetAllBooks()
+        {
+            var getRequest = new RestRequest("book", Method.Get);
+            var getResponse = client.Execute(getRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+                Assert.That(getResponse.Content, Is.Not.Null.Or.Empty, "respon content not ok");
+
+                var books = JArray.Parse(getResponse.Content);
+
+                Assert.That(books.Type, Is.EqualTo(JTokenType.Array), "content not jarray");
+                Assert.That(books.Count, Is.GreaterThan(0), "less than zero");
+
+                foreach (var book in books)
+                {
+                    Assert.That(book["title"]?.ToString(), Is.Not.Null.Or.Empty);
+                    Assert.That(book["author"]?.ToString(), Is.Not.Null.Or.Empty);
+                    Assert.That(book["description"]?.ToString(), Is.Not.Null.Or.Empty);
+                    Assert.That(book["price"]?.ToString(), Is.Not.Null.Or.Empty);
+                    Assert.That(book["pages"]?.ToString(), Is.Not.Null.Or.Empty);
+                    Assert.That(book["category"]?.ToString(), Is.Not.Null.Or.Empty);
+                }
+            });
+        }
+
+        [Test]
+        public void Test_GetBookByTitle()
+        {
+            var getRequest = new RestRequest("book", Method.Get);
+            var getResponse = client.Execute(getRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+                Assert.That(getResponse.Content, Is.Not.Null.Or.Empty, "respon content not ok");
+
+                var books = JArray.Parse(getResponse.Content);
+                var book = books.FirstOrDefault(b => b["title"]?.ToString() == "The Great Gatsby");
+
+                Assert.That(book["author"]?.ToString(), Is.EqualTo("F. Scott Fitzgerald"));
+            });
+        }
+
+        [Test]
+        public void Test_AddBook()
+        {
+            var getCategoriesRequest = new RestRequest("category", Method.Get);
+            var getCategoriesResponse = client.Execute(getCategoriesRequest);
+
+            var categories = JArray.Parse(getCategoriesResponse.Content);
+            var firstCategory = categories.First();
+            var categoryId = firstCategory["_id"]?.ToString();
+
+            var addRequest = new RestRequest("book", Method.Post);
+            addRequest.AddHeader("Authorization", $"Bearer {token}");
+
+            var title = "Random Title";
+            var author = "Random author";
+            var description = "random description";
+            var price = 10;
+            var pages = 100;
+
+            addRequest.AddJsonBody(new
+            {
+                title,
+                author,
+                description,
+                price,
+                pages,
+                category = categoryId
+            });
+
+            var addResponse = client.Execute(addRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(addResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+                Assert.That(addResponse.Content, Is.Not.Null.Or.Empty, "respon content not ok");
+            });
+
+            var createdBookId = JObject.Parse(addResponse.Content);
+            Assert.That(createdBookId["_id"]?.ToString(), Is.Not.Empty); 
+
+            var createdID = createdBookId["_id"]?.ToString();
+            var bookCategoryId = createdBookId["category"]["_id"]?.ToString();
+
+            var getBookRequest = new RestRequest($"/book/{createdID}", Method.Get);
+            var getResponse = client.Execute(getBookRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+                Assert.That(getResponse.Content, Is.Not.Null.Or.Empty, "respon content not ok");
+            });
+
+            var book = JObject.Parse(getResponse.Content);
+
+            Assert.That(book["title"]?.ToString(), Is.EqualTo("Random Title"));
+            Assert.That(book["author"]?.ToString(), Is.EqualTo("Random author"));
+            Assert.That(book["description"]?.ToString(), Is.EqualTo("random description"));
+            Assert.That(book["price"]?.ToString(), Is.EqualTo("10"));
+            Assert.That(book["pages"]?.ToString(), Is.EqualTo("100"));
+            Assert.That(book["category"]?.ToString(), Is.Not.Empty);
+
+            Assert.That(book["category"]["_id"]?.ToString(), Is.EqualTo(bookCategoryId));
+        }
+
+        [Test]
+        public void Test_UpdateBook()
+        {
+            var getRequest = new RestRequest("book", Method.Get);
+            var getResponse = client.Execute(getRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+                Assert.That(getResponse.Content, Is.Not.Null.Or.Empty, "respon content not ok");
+            });
+
+            var books = JArray.Parse(getResponse.Content);
+            var bookToUpdate = books.FirstOrDefault(b => b["title"]?.ToString() == "The Catcher in the Rye");
+
+            Assert.That(bookToUpdate, Is.Not.Null);
+
+            var bookId = bookToUpdate["_id"]?.ToString();
+
+            var updateRequest = new RestRequest($"book/{bookId}", Method.Put);
+            updateRequest.AddHeader("Authorization", $"Bearer {token}");
+
+            updateRequest.AddJsonBody(new
+            {
+                title = "Updated Book Title",
+                author = "Updated Author"
+            });
+
+            var updateResponse = client.Execute(updateRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(updateResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+                Assert.That(updateResponse.Content, Is.Not.Null.Or.Empty, "respon content not ok");
+
+                var updatedBook = JObject.Parse(updateResponse.Content);
+
+                Assert.That(updatedBook["title"]?.ToString(), Is.EqualTo("Updated Book Title"));
+                Assert.That(updatedBook["author"]?.ToString(), Is.EqualTo("Updated Author"));
+            });
+        }
+
+        [Test]
+        public void Test_DeleteBook()
+        {
+            var getRequest = new RestRequest("book", Method.Get);
+            var getResponse = client.Execute(getRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(getResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+                Assert.That(getResponse.Content, Is.Not.Null.Or.Empty, "respon content not ok");
+            });
+
+            var books = JArray.Parse(getResponse.Content);
+            var bookToDelete = books.FirstOrDefault(b => b["title"]?.ToString() == "To Kill a Mockingbird");
+
+            Assert.That(bookToDelete, Is.Not.Null);
+
+            var bookId = bookToDelete["_id"]?.ToString();
+
+            var deleteRequest = new RestRequest($"book/{bookId}", Method.Delete);
+            deleteRequest.AddHeader("Authorization", $"Bearer {token}");
+
+            var deleteResponse = client.Execute(deleteRequest);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(deleteResponse.StatusCode, Is.EqualTo(HttpStatusCode.OK), "Status code not OK");
+
+                var verifyRequset = new RestRequest($"book/{bookId}");
+                var verifyResponse = client.Execute(verifyRequset);
+
+                Assert.That(verifyResponse.Content, Is.EqualTo("null"));
+            });
+
+
+        }
+
+        public void Dispose()
+        {
+            client?.Dispose();
+        }
+    }
+}
